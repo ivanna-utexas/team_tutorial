b14548e3f1093fdfaacaa31c158a440b02d6870a
Samyak Singh <samyak@cs.utexas.edu>
2026-01-22 17:20:16 -0600

    node_b.py creation

--- setup.py
+++ setup.py
@@ -24,6 +24,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'node_a = team_tutorial.node_a:main',
+            'node_b = team_tutorial.node_b:main',
         ],
     },
 )
